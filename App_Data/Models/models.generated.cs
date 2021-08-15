3b36eac92e2531c5a60c2abd858ac73ce9964bd7
MatijaKocevar <matija.kocev@gmail.com>
2021-08-15 22:52:48 +0200

    Created partial views & client dependencies

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "39a1473e383f808")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedModels
 {
